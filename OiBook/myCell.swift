a2a649a2307b5a6b060e4d8ff892d6fdbeb4d20b
hoanghavb <habh.hpit@gmail.com>
2017-08-02 16:12:09 +0700

    first commit project

---
+++ OiBook/myCell.swift
@@ -0,0 +1,13 @@
+//
+//  myCell.swift
+//  OiBook
+//
+//  Created by Bùi Hà on 8/1/17.
+//  Copyright © 2017 Bùi Hà. All rights reserved.
+//
+
+import UIKit
+
+class myCell: UICollectionViewCell {
+    
+}
